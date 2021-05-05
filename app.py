8265ea430a9939f3f54442f02e789b924aed081d
inkarar <78466887+inkarar@users.noreply.github.com>
2021-05-05 21:49:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,127 +34,121 @@ detector = cv2.CascadeClassifier(cascade)
 print("[INFO] starting video stream...")
 st.write("[INFO] starting video stream...")
 
-try:
-    vs = VideoStream(src=0).start()
-    #vs = VideoStream(usePiCamera=True).start()
-    time.sleep(1.0)
+vs = VideoStream(src=0).start()
+#vs = VideoStream(usePiCamera=True).start()
+time.sleep(1.0)
 
-    # start the FPS counter
-    fps = FPS().start()
+# start the FPS counter
+fps = FPS().start()
 
-    # loop over frames from the video file stream
-    names = []
-    while True:
-        # grab the frame from the threaded video stream and resize it
-        # to 500px (to speedup processing)
-        frame = vs.read()
-        frame = imutils.resize(frame, width=500)
-        
-        # convert the input frame from (1) BGR to grayscale (for face
-        # detection) and (2) from BGR to RGB (for face recognition)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+# loop over frames from the video file stream
+names = []
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to 500px (to speedup processing)
+    frame = vs.read()
+    frame = imutils.resize(frame, width=500)
 
-        # detect faces in the grayscale frame
-        rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
-            minNeighbors=5, minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE)
+    # convert the input frame from (1) BGR to grayscale (for face
+    # detection) and (2) from BGR to RGB (for face recognition)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # OpenCV returns bounding box coordinates in (x, y, w, h) order
-        # but we need them in (top, right, bottom, left) order, so we
-        # need to do a bit of reordering
-        boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+    # detect faces in the grayscale frame
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+        minNeighbors=5, minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE)
 
-        # compute the facial embeddings for each face bounding box
-        encodings = face_recognition.face_encodings(rgb, boxes)
-        #names = []
+    # OpenCV returns bounding box coordinates in (x, y, w, h) order
+    # but we need them in (top, right, bottom, left) order, so we
+    # need to do a bit of reordering
+    boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
 
-        # loop over the facial embeddings
-        for encoding in encodings:
-            # attempt to match each face in the input image to our known
-            # encodings
-            matches = face_recognition.compare_faces(data["encodings"],
-                encoding)
-            name = "Unknown" #if face is not recognized, then print Unknown
+    # compute the facial embeddings for each face bounding box
+    encodings = face_recognition.face_encodings(rgb, boxes)
+    #names = []
 
-            # check to see if we have found a match
-            if True in matches:
-                # find the indexes of all matched faces then initialize a
-                # dictionary to count the total number of times each face
-                # was matched
-                matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-                counts = {}
+    # loop over the facial embeddings
+    for encoding in encodings:
+        # attempt to match each face in the input image to our known
+        # encodings
+        matches = face_recognition.compare_faces(data["encodings"],
+            encoding)
+        name = "Unknown" #if face is not recognized, then print Unknown
 
-                # loop over the matched indexes and maintain a count for
-                # each recognized face face
-                for i in matchedIdxs:
-                    name = data["names"][i]
-                    counts[name] = counts.get(name, 0) + 1
+        # check to see if we have found a match
+        if True in matches:
+            # find the indexes of all matched faces then initialize a
+            # dictionary to count the total number of times each face
+            # was matched
+            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
 
-                # determine the recognized face with the largest number
-                # of votes (note: in the event of an unlikely tie Python
-                # will select first entry in the dictionary)
-                name = max(counts, key=counts.get)
+            # loop over the matched indexes and maintain a count for
+            # each recognized face face
+            for i in matchedIdxs:
+                name = data["names"][i]
+                counts[name] = counts.get(name, 0) + 1
 
-                
-                #If someone in your dataset is identified, print their name on the screen
-                #if currentname != name:
-                    #currentname = name
-                    #print(name)
-            
-            # update the list of names
-            names.append(name)
-
-        # loop over the recognized faces
-        for ((top, right, bottom, left), name) in zip(boxes, names):
-            # draw the predicted face name on the image - color is in BGR
-            cv2.rectangle(frame, (left, top), (right, bottom),
-                (0, 255, 0), 2)
-            y = top - 15 if top - 15 > 15 else top + 15
-            cv2.putText(frame, max(set(names), key = names.count), (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                .8, (255, 0, 0), 2)
-
-        # display the image to our screen
-        cv2.imshow("Facial Recognition is Running", frame)
-        key = cv2.waitKey(1) & 0xFF
-
-        # quit when 'q' key is pressed
-        if key == ord("q") or len(names)==30:
-            break
-
-        # update the FPS counter
-        fps.update()
+            # determine the recognized face with the largest number
+            # of votes (note: in the event of an unlikely tie Python
+            # will select first entry in the dictionary)
+            name = max(counts, key=counts.get)
 
 
-    #print name of person identified and accuracy
-    #max([(name,names.count(name)) for name in set(names)],key=lambda x:x[1])
-    print("person identified : ", max(set(names), key = names.count))
-    print([(name,names.count(name)) for name in set(names)])
+            #If someone in your dataset is identified, print their name on the screen
+            #if currentname != name:
+                #currentname = name
+                #print(name)
 
-    st.write(names)
+        # update the list of names
+        names.append(name)
 
-    st.write("person identified : ", max(set(names), key = names.count))
-    st.write([(name,names.count(name)) for name in set(names)][0])
+    # loop over the recognized faces
+    for ((top, right, bottom, left), name) in zip(boxes, names):
+        # draw the predicted face name on the image - color is in BGR
+        cv2.rectangle(frame, (left, top), (right, bottom),
+            (0, 255, 0), 2)
+        y = top - 15 if top - 15 > 15 else top + 15
+        cv2.putText(frame, max(set(names), key = names.count), (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+            .8, (255, 0, 0), 2)
+
+    # display the image to our screen
+    cv2.imshow("Facial Recognition is Running", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # quit when 'q' key is pressed
+    if key == ord("q") or len(names)==30:
+        break
+
+    # update the FPS counter
+    fps.update()
+
+
+#print name of person identified and accuracy
+#max([(name,names.count(name)) for name in set(names)],key=lambda x:x[1])
+print("person identified : ", max(set(names), key = names.count))
+print([(name,names.count(name)) for name in set(names)])
+
+st.write(names)
+
+st.write("person identified : ", max(set(names), key = names.count))
+st.write([(name,names.count(name)) for name in set(names)][0])
 
 
 
-    # stop the timer and display FPS information
-    fps.stop()
-    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-    st.write("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-    st.write("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-
-    # do a bit of cleanup
-    cv2.destroyAllWindows()
-    vs.stop()    
+st.write("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+st.write("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
 
-except:
-    print("no image captured")
-    st.write("no image captured")
-    print("something went wrong")
-    st.write("something went wrong")
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()    
+
+
 
